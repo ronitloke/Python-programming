303acb8965f27e2baeb236b5b3bc0ebab72d1063
ronitloke <82684588+ronitloke@users.noreply.github.com>
2026-02-11 23:09:57 +0000

    Add files via upload

---
+++ Code With Harry- Python/Day-016/Match case Month.py
@@ -0,0 +1,31 @@
+x= "jul"
+
+match x:
+    case "jan":
+        print("current month is jan")
+    case "feb":
+        print("current month is feb")
+    case "mar":
+        print("current month is mar")
+    case "apr":
+        print("current month is apr")
+    case "may":
+        print("current month is may")
+    case "jun":
+        print("current month is jun")
+    case "jul":
+        print("current month is jul123")
+    case "aug":
+        print("current month is aug")
+    case "sept":
+        print("current month is sept")
+    case "oct":
+        print("current month is oct")
+    case "nov":
+        print("current month is nov")
+    case "dec":
+        print("current month is dec")
+    case _:
+        print("Not a Month")
+
+    
---
+++ Code With Harry- Python/Day-016/Matchcase current month.py
@@ -0,0 +1,36 @@
+import datetime
+
+x= datetime.datetime.now()
+print(x)
+z=x.strftime("%b").lower()
+print(z)
+
+match z:
+    case "jan":
+        print("current month is jan")
+    case "feb":
+        print("current month is feb")
+    case "mar":
+        print("current month is mar")
+    case "apr":
+        print("current month is apr")
+    case "may":
+        print("current month is may")
+    case "jun":
+        print("current month is jun")
+    case "jul":
+        print("current month is jul")
+    case "aug":
+        print("current month is aug")
+    case "sept":
+        print("current month is sept")
+    case "oct":
+        print("current month is oct")
+    case "nov":
+        print("current month is nov")
+    case "dec":
+        print("current month is dec")
+    case _:
+        print("Not a Month")
+
+    
