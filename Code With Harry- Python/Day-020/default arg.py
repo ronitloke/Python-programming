4b59da543617665b4409646fa91d9ce089d4fb22
ronitloke <82684588+ronitloke@users.noreply.github.com>
2026-02-16 18:48:23 +0000

    Add files via upload

---
+++ Code With Harry- Python/Day-020/default arg.py
@@ -0,0 +1,11 @@
+# def average(a=5, b=10):
+#     c=(a+b)/2
+#     print(c)
+#
+# average()
+
+
+def identity(fname, mname= "devendra", lname= "loke"):
+    print("hello", fname, mname, lname)
+
+identity("Ronit", "dev", "lok")
