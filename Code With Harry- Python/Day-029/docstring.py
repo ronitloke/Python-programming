9561f082d6e339d644a5bc0a2c732f1aeb84d7f4
ronitloke <82684588+ronitloke@users.noreply.github.com>
2026-03-01 11:38:39 +0000

    Add files via upload

---
+++ Code With Harry- Python/Day-029/docstring.py
@@ -0,0 +1,7 @@
+def square(n):
+    '''Takes the no as input'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+#Doc string should always be below function name or else it is not considered as doc string
