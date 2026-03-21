a97ed9a4aef9c95a4bc4c624b78e7fba7c63139c
ronitloke <82684588+ronitloke@users.noreply.github.com>
2026-03-21 11:47:10 +0000

    Add files via upload

---
+++ Code With Harry- Python/Practice/p76.py
@@ -0,0 +1,7 @@
+prices={"IBM":678.9,"persistent":515.8,"sony":200.8,"microland":313.7,"gajshield":400.8}
+
+d={}
+for key,value in prices.items():
+    if(value>=300):
+        d[key]=value
+print(d)
