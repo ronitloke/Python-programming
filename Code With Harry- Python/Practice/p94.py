06e80842ad623d8e2da74ab93b7887475af2901b
ronitloke <82684588+ronitloke@users.noreply.github.com>
2026-03-29 22:33:15 +0100

    Add files via upload

---
+++ Code With Harry- Python/Practice/p94.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if isinstance(n,str):
+        raise BaseException("Any string value is not allowed")
+    if n<0:
+        raise ValueError("The num should not be less than zero")
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(-1))
