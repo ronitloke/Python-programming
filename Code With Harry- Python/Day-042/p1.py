983827d4c4cb991ceb8351f579d8b785ab900825
ronitloke <82684588+ronitloke@users.noreply.github.com>
2026-04-10 22:09:41 +0100

    Add files via upload

---
+++ Code With Harry- Python/Day-042/p1.py
@@ -0,0 +1,3 @@
+marks=[23,45,21,3,45,6]
+for index,mark in enumerate(marks):
+    print(index,mark)
---
+++ Code With Harry- Python/Day-042/p3.py
@@ -0,0 +1,3 @@
+marks=[23,45,21,3,45,6]
+for index,mark in enumerate(marks,start=1):
+    print(index,mark)
