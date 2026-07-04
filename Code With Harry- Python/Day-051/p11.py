4c15ef607b1892e490f1499a5a05036b616c5aa8
Ronit Loke <82684588+ronitloke@users.noreply.github.com>
2026-07-04 14:15:39 +0100

    Add files via upload

---
+++ Code With Harry- Python/Day-051/p11.py
@@ -0,0 +1,13 @@
+def func1():
+    word = "learning"
+    data = True
+    line_no = 1
+    with open("practice.txt","r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+                return
+            line_no +=1
+    return -1
+print(func1())
